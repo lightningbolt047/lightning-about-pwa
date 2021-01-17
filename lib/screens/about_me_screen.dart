f39cb6a19cb64b2a9914f6b11e6816873822333d
Sashank Visweshwaran <sashankvisweshwaran@gmail.com>
2021-01-17 18:32:30 +0400

    Made MySkills UI responsive

--- lib/screens/about_me_screen.dart
+++ lib/screens/about_me_screen.dart
@@ -1,4 +1,5 @@
 import 'package:community_material_icon/community_material_icon.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:myresume/const.dart';
@@ -42,14 +43,14 @@ class _AboutMeState extends State<AboutMe> with SingleTickerProviderStateMixin {
     return Scaffold(
       backgroundColor: kFinalScaffoldColor,
       appBar: AppBar(
-        title: Text("Skills"),
+        title: Text("Skills",style: TextStyle(fontWeight: FontWeight.bold),),
         backgroundColor: kFinalScaffoldColor,
         centerTitle: true,
       ),
       body: TabBarView(
         controller: _tabController,
         children: [
-          MySkills(_lowWidth,_fullSize),
+          MySkills(),
           MyProjects(),
           ContactMe(),
         ],
@@ -64,27 +65,64 @@ class _AboutMeState extends State<AboutMe> with SingleTickerProviderStateMixin {
 
 class MySkills extends StatefulWidget {
 
-  final Size _fullSize;
-  final bool _lowWidth;
-
-  MySkills(this._lowWidth,this._fullSize);
-
   @override
-  _MySkillsState createState() => _MySkillsState(this._lowWidth,this._fullSize);
+  _MySkillsState createState() => _MySkillsState();
 }
 
 class _MySkillsState extends State<MySkills> with SingleTickerProviderStateMixin {
   AnimationController _controller;
 
-  Size _fullSize;
-  bool _lowWidth;
+  bool _lowWidth=false;
 
-  _MySkillsState(this._lowWidth,this._fullSize);
+  Animation<Offset> _stackNameSlideAnimation;
+  Animation<Offset> _frameworkSlideAnimation;
+  Animation<Offset> _otherSkillsAnimation;
+
+
+
+  void _isLowWidth(){
+    if(MediaQuery.of(context).size.width<600){
+      _lowWidth=true;
+    }
+    return;
+  }
+
+
 
   @override
   void initState() {
-    _controller = AnimationController(vsync: this);
+    _controller = AnimationController(vsync: this, duration: Duration(seconds: 1,milliseconds: 150))..addListener(() {
+      setState(() {});
+    });
+
+    _stackNameSlideAnimation=Tween<Offset>(
+      begin: Offset(0.25,0),
+      end: Offset(0,0),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut
+    ));
+
+    _frameworkSlideAnimation=Tween<Offset>(
+      begin: Offset(-0.25,0),
+      end: Offset(0,0),
+    ).animate(CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut
+    ));
+
+    _otherSkillsAnimation=Tween<Offset>(
+      begin: Offset(0,-0.25),
+      end: Offset(0,0)
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut
+    ));
+
+    _controller.forward();
+
     super.initState();
+
   }
 
   @override
@@ -95,173 +133,193 @@ class _MySkillsState extends State<MySkills> with SingleTickerProviderStateMixin
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+
+    Size _fullSize=MediaQuery.of(context).size;
+    _isLowWidth();
+
+
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          child: Center(
+            child: Column(
               children: [
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  width: _fullSize.width*0.5,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Card(
-                        elevation: 0,
-                        color: kFinalScaffoldColor,
-                        child: Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Text("FrontEnd",style: TextStyle(color: Colors.white,fontSize: 30),),
+                      SlideTransition(
+                        position: _stackNameSlideAnimation,
+                        child: Container(
+                          padding: const EdgeInsets.all(16.0),
+                          width: _fullSize.width*0.5,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Card(
+                                elevation: 0,
+                                color: kFinalScaffoldColor,
+                                child: Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: Text("FrontEnd",style: TextStyle(color: Colors.white,fontSize: 30),),
+                                ),
+                              ),
+                              SizedBox(
+                                height: _fullSize.height*0.1,
+                              ),
+                              Card(
+                                elevation: 0,
+                                color: kFinalScaffoldColor,
+                                child: Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: Text("BackEnd",style: TextStyle(color: Colors.white,fontSize: 30),),
+                                ),
+                              ),
+                              SizedBox(
+                                height: _fullSize.height*0.1,
+                              ),
+                              Card(
+                                elevation: 0,
+                                color: kFinalScaffoldColor,
+                                child: Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: Text("Database",style: TextStyle(color: Colors.white,fontSize: 30),),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                      SizedBox(
-                        height: _fullSize.height*0.1,
+
+                      VerticalDivider(
+                        width: 1,
+                        thickness: 1,
+                        color: Colors.white,
+                        indent: 40,
+                        endIndent: 40,
                       ),
-                      Card(
-                        elevation: 0,
-                        color: kFinalScaffoldColor,
-                        child: Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Text("BackEnd",style: TextStyle(color: Colors.white,fontSize: 30),),
-                        ),
-                      ),
-                      SizedBox(
-                        height: _fullSize.height*0.1,
-                      ),
-                      Card(
-                        elevation: 0,
-                        color: kFinalScaffoldColor,
-                        child: Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Text("DB",style: TextStyle(color: Colors.white,fontSize: 30),),
+                      SlideTransition(
+                        position: _frameworkSlideAnimation,
+                        child: Container(
+                          padding: const EdgeInsets.all(16.0),
+                          width: _fullSize.width*0.45,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Image.asset('assets/flutter_icon.png',fit: BoxFit.contain,color: Colors.white,scale: 2.2,),
+                                      Text("Flutter",style: TextStyle(color: Colors.white),)
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: _fullSize.width*0.02,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Icon(FontAwesomeIcons.html5,color: Colors.white,size: 60,),
+                                      Text("HTML",style: TextStyle(color: Colors.white),)
+                                    ],
+                                  ),
+
+                                ],
+                              ),
+                              SizedBox(
+                                height: _fullSize.height*0.08,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Icon(CommunityMaterialIcons.nodejs,color: Colors.white,size: 60,),
+                                      Text("NodeJS",style: TextStyle(color: Colors.white),)
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: _fullSize.width*0.02,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Icon(CommunityMaterialIcons.language_php,color: Colors.white,size: 60,),
+                                      Text("PHP",style: TextStyle(color: Colors.white),)
+                                    ],
+                                  ),
+
+                                ],
+                              ),
+                              SizedBox(
+                                height: _fullSize.height*0.08,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Icon(CommunityMaterialIcons.firebase,color: Colors.white,size: _lowWidth?35:60,),
+                                      Text("Firebase",style: TextStyle(color: Colors.white,fontSize: _lowWidth?8:15),),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: _lowWidth?_fullSize.width*0.01:_fullSize.width*0.02,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Image.asset('assets/mongodb_icon.png',fit: BoxFit.contain,color: Colors.white,scale: _lowWidth?4:2.2,),
+                                      Text("MongoDB",style: TextStyle(color: Colors.white,fontSize: _lowWidth?8:15),),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: _lowWidth?_fullSize.width*0.01:_fullSize.width*0.02,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Image.asset('assets/sql_icon.png',fit: BoxFit.contain,color: Colors.white,scale: _lowWidth?4:2.2,),
+                                      Text("SQL",style: TextStyle(color: Colors.white,fontSize: _lowWidth?8:15),),
+                                    ],
+                                  ),
+                                ],
+                              ),
+
+                            ],
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
-
-                VerticalDivider(
-                  width: 1,
-                  thickness: 1,
-                  color: Colors.white,
-                  indent: 40,
-                  endIndent: 40,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  width: _fullSize.width*0.45,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Image.asset('icon_pics/flutter.png',fit: BoxFit.contain,color: Colors.white,scale: 2.2,),
-                              Text("Flutter",style: TextStyle(color: Colors.white),)
-                            ],
-                          ),
-                          SizedBox(
-                            width: _fullSize.width*0.02,
-                          ),
-                          Column(
-                            children: [
-                              Icon(FontAwesomeIcons.html5,color: Colors.white,size: 60,),
-                              Text("HTML",style: TextStyle(color: Colors.white),)
-                            ],
-                          ),
-
-                        ],
-                      ),
-                      SizedBox(
-                        height: _fullSize.height*0.08,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Icon(CommunityMaterialIcons.nodejs,color: Colors.white,size: 60,),
-                              Text("NodeJS",style: TextStyle(color: Colors.white),)
-                            ],
-                          ),
-                          SizedBox(
-                            width: _fullSize.width*0.02,
-                          ),
-                          Column(
-                            children: [
-                              Icon(CommunityMaterialIcons.language_php,color: Colors.white,size: 60,),
-                              Text("PHP",style: TextStyle(color: Colors.white),)
-                            ],
-                          ),
-
-                        ],
-                      ),
-                      SizedBox(
-                        height: _fullSize.height*0.08,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Column(
-                            children: [
-                              Icon(CommunityMaterialIcons.firebase,color: Colors.white,size: _lowWidth?35:60,),
-                              Text("Firebase",style: TextStyle(color: Colors.white,fontSize: _lowWidth?8:15),),
-                            ],
-                          ),
-                          SizedBox(
-                            width: _lowWidth?_fullSize.width*0.01:_fullSize.width*0.02,
-                          ),
-                          Column(
-                            children: [
-                              Image.asset('icon_pics/mongodb.png',fit: BoxFit.contain,color: Colors.white,scale: _lowWidth?4:2.2,),
-                              Text("MongoDB",style: TextStyle(color: Colors.white,fontSize: _lowWidth?8:15),),
-                            ],
-                          ),
-                          SizedBox(
-                            width: _lowWidth?_fullSize.width*0.01:_fullSize.width*0.02,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Image.asset('icon_pics/sql.png',fit: BoxFit.contain,color: Colors.white,scale: _lowWidth?4:2.2,),
-                              Text("SQL",style: TextStyle(color: Colors.white,fontSize: _lowWidth?8:15),),
-                            ],
-                          ),
-                        ],
-                      ),
-
-                    ],
+                SlideTransition(position: _otherSkillsAnimation,child: Text("Other Skills",style: TextStyle(color: Colors.white,fontSize: 25),)),
+                SlideTransition(
+                  position: _otherSkillsAnimation,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(FontAwesomeIcons.python,color: Colors.white,size: 60,),
+                        SizedBox(
+                          width: _fullSize.width*0.05,
+                        ),
+                        Icon(FontAwesomeIcons.java,color: Colors.white,size: 60,),
+                        SizedBox(
+                          width: _fullSize.width*0.05,
+                        ),
+                        Icon(CommunityMaterialIcons.language_cpp,color: Colors.white,size: 60,),
+                      ],
+                    ),
                   ),
-                ),
+                )
               ],
             ),
           ),
-          Text("Other Skills",style: TextStyle(color: Colors.white,fontSize: 25),),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(FontAwesomeIcons.python,color: Colors.white,size: 60,),
-                SizedBox(
-                  width: _fullSize.width*0.05,
-                ),
-                Icon(FontAwesomeIcons.java,color: Colors.white,size: 60,),
-                SizedBox(
-                  width: _fullSize.width*0.05,
-                ),
-                Icon(CommunityMaterialIcons.language_cpp,color: Colors.white,size: 60,),
-              ],
-            ),
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
