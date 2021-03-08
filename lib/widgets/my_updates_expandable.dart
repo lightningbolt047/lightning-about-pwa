a4256c6903e149b229d52822bd8a9ffbdd4ee915
Sashank <sashankvisweshwaran@gmail.com>
2021-03-08 08:04:32 +0530

    Updated the resume pdf

--- lib/widgets/my_updates_expandable.dart
+++ lib/widgets/my_updates_expandable.dart
@@ -1,7 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:expandable/expandable.dart';
-import 'package:myresume/const.dart';
 
 
 class MyUpdateExpandable extends StatefulWidget {
