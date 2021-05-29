22fdeddf8d4eb7e2c6b90dec2112006753e9e239
Sashank <sashankvisweshwaran@gmail.com>
2021-05-29 15:34:01 +0530

    Changed version strings

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -101,7 +101,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                           context: context,
                           builder: (context)=>AlertDialog(
                             title: Text("Alert"),
-                            content: Text("Built with Flutter!\nFlutter: 2.0.5"),
+                            content: Text("Built with Flutter!\nFlutter: 2.2.1"),
                             contentPadding: EdgeInsets.all(16),
                             actionsPadding: EdgeInsets.all(8),
                             actions: [
